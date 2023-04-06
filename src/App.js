ec63db9ea86439ecffc22050da3c0c4cebe5f558
IshanMK <e18173@eng.pdn.ac.lk>
2023-04-06 12:26:33 +0530

    Final Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-// import logo from "./logo.svg";
+// import logo from './logo.svg';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>ishanmk.github.io</p>
+        <p>Ishan Maduranga</p>
+        <a
+          href="https://twitter.com/ishan_mk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow Me
+        </a>
       </header>
     </div>
   );
