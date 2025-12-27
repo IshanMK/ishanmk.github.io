ddc22ba138ffbd6e32447a3ed91af68bf3662ca4
IshanMK <e18173@eng.pdn.ac.lk>
2025-12-27 14:51:48 +0530

    updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <h1>Ishan Madhuranga</h1>;
+  return (
+    <main
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <header>
+        <h1>Ishan Madhuranga</h1>
+      </header>
+    </main>
+  );
 }
 
 export default App;
